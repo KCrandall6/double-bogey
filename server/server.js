7073030e44be084fb1a8da8f21cc149aa44f95c0
KCrandall6 <kcrandall67@gmail.com>
2023-05-06 17:21:06 -0700

    boiler plate app complete

---
+++ server/server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const PORT = 3000;
+// const endpoints = require('./routers/endpoints');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static(path.join(__dirname, '../build')));
+
+// app.use('/', endpoints);
+
+app.use((req, res) =>
+  res.status(404).send("This is not the page you're looking for...")
+);
+
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is listening on port: http://localhost:${PORT}`);
+});
+
+module.exports = app;
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: ['babel-polyfill', './client/index.js'],
+  output: {
+    path: path.resolve('build'),
+    filename: 'bundle.js',
+  },
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({ template: './dev.html' }),
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+        {
+            test: /\.css$/,
+            use: [
+                process.env.NODE_ENV === 'production'
+                  ? MiniCssExtractPlugin.loader
+                  : 'style-loader',
+                'css-loader'
+            ]
+        },
+        {
+            test: /\.(jsx?)$/,
+            exclude: /node_modules/,
+            use: {
+              loader: 'babel-loader',
+              options: {
+                presets: ['@babel/preset-env', '@babel/preset-react'],
+              },
+            },
+          },
+          {
+            test: /\.(png|jpg|gif)$/i,
+            use: [
+              {
+                loader: 'file-loader',
+              },
+            ],
+          },
+          {
+            test: /\.sass$/i,
+            exclude: /node_modules/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader',
+            ],
+          }
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  devServer: {
+    compress: true,
+    port: 8080,
+    hot: true,
+    historyApiFallback: true,
+    proxy: {
+      '/api': 'http://localhost:3000',
+      secure: true,
+    },
+    static: {
+      directory: path.join(__dirname, '/'),
+      publicPath: '/',
+    },
+  },
+};
