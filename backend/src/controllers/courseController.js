302aa032aad0b276aa1ee255498cfb5c9f7d8d39
KCrandall6 <kcrandall67@gmail.com>
2023-05-15 00:21:16 -0700

    reformatted backend to support netlify api creation

--- backend/src/controllers/courseController.js
+++ backend/src/controllers/courseController.js
@@ -3,6 +3,7 @@ const db = require('../models/doublebogeyModel');
 const courseController = {};
 
 courseController.getCourseInfo = (req, res, next) => {
+  console.log('in the middleware', req.query)
   const course = req.query.course;
   const searchQuery = `SELECT * FROM "public.Courses" WHERE "courseName" = '${course}'`;
 
