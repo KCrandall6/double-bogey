a959e5f9d000292863f175af299e132743bd3d3e
KCrandall6 <kcrandall67@gmail.com>
2023-05-23 17:26:06 -0700

    updated logic for netlify function to first check database if player exists, add to database, then add game data

--- double-bogey/functions/addNewGame.js
+++ double-bogey/functions/addNewGame.js
@@ -2,19 +2,34 @@ const db = require('../doublebogeyModel');
 
 exports.handler = async function (event, context) {
   try {
-
-    console.log('on the back', JSON.parse(event.body))
-    // Parse the incoming data from the request body
+    console.log('on the back', JSON.parse(event.body));
     const { name, course, date, scores, total } = JSON.parse(event.body);
 
-    // Construct the query to insert the game data
-    const queryString = `
-      INSERT INTO "public.Games" ("player", "course", "date", "scores", "total")
-      VALUES ($1, $2, $3, $4, $5)
-    `;
-    const queryValues = [name, course, date, scores, total];
+    if (!name) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({ error: 'Player name is required' }),
+      };
+    }
 
-    // Execute the query using the database connection
+    const playerResult = await db.query('SELECT * FROM "public.Players" WHERE "player_name" = $1', [name]);
+
+    let playerId;
+    if (playerResult.rows.length === 0) {
+      const playerInsertResult = await db.query('INSERT INTO "public.Players" ("player_name") VALUES ($1) RETURNING "player_id"', [name]);
+      playerId = playerInsertResult.rows[0].player_id;
+    } else {
+      playerId = playerResult.rows[0].player_id;
+    }
+
+    const queryString = `
+      INSERT INTO "public.Games" ("player_id", "player", "course", "date", "scores", "total")
+      SELECT $1, $2, $3, $4, $5, $6
+      FROM "public.Players"
+      WHERE "public.Players"."player_id" = $1
+    `;
+    const queryValues = [playerId, name, course, date, scores, total];
+
     const result = await db.query(queryString, queryValues);
 
     return {
