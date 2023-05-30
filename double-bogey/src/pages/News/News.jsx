88eae597109b49a5cc142aa4e0bba16cbea501d7
KCrandall6 <kcrandall67@gmail.com>
2023-05-30 15:53:47 -0700

    completed news page to display both recent articles and videos in both desktop and mobile view

--- double-bogey/src/pages/News/News.jsx
+++ double-bogey/src/pages/News/News.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
-import { Button, Card, Container } from 'react-bootstrap';
+import { Card, Container } from 'react-bootstrap';
+
+import NewsModal from './NewsModal';
 
 const News = () => {
 
   const [articles, setArticles] = useState([]);
-  // const today = new Date().toJSON();
+  const today = new Date();
+  console.log('today', today)
 
   useEffect(() => {
     fetch('https://site.api.espn.com/apis/site/v2/sports/golf/pga/news')
@@ -12,30 +15,36 @@ const News = () => {
     .then((res) => setArticles(res.articles))
   }, [])
 
-  console.log('articles', articles)
+  const formatTimestamp = (timestamp) => {
+    const date = new Date(timestamp);
+    const options = { year: 'numeric', month: 'long', day: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+  };
 
   return (
-    <div>
-      <h1>Golf News</h1>
+    <div className="news-container">
+      <div className="d-flex flex-column align-items-center justify-content-center text-center pt-3">
+        <h1>Latest Golf News</h1>
+        <h4>{formatTimestamp(today)}</h4>
+      </div>
       {articles.map((article) => {
         return (
-          <Container key={article.dataSourceIdentifier} className="d-flex align-items-center justify-content-center text-start pt-3">
+          <Container key={article.dataSourceIdentifier} className="d-flex align-items-center justify-content-center text-start pt-3" style={{maxWidth: "750px"}}>
             <Card border="dark">
               <Card.Img className="pt-3 ps-3 pe-3" src={article.images[0].url} />
-              <Card.Body>
-                <Card.Title>{article.headline}</Card.Title>
-                <Card.Text>
+              <Card.Body className="d-flex flex-column">
+                <h2>{article.headline}</h2>
+                <h6 className="mt-1"><em>~ {formatTimestamp(article.published)}</em></h6>
+                <Card.Text className="mt-3 mb-4">
                   {article.description}
                 </Card.Text>
-                <Button style={{ color:"white", backgroundColor: "#395144", border: "none"}}>Read More...</Button>
+    
+                <NewsModal url={article.links.api.news} formatTimestamp={formatTimestamp}/>
               </Card.Body>
             </Card>
           </Container>
         )
       })}
-      {/* <div className='pb-5'>
-        <p>l</p>
-      </div> */}
     </div>
   )
 }
---
+++ double-bogey/src/pages/News/NewsModal.jsx
@@ -0,0 +1,90 @@
+import React, {useState, useEffect} from 'react'; 
+import { Modal, Button} from 'react-bootstrap';
+
+const NewsModal = ({url, formatTimestamp}) => {
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const [content, setContent] = useState({})
+  useEffect(() => {
+    fetch(url.href)
+    .then((res) => res.json())
+    .then((res) => setContent(res))
+  }, [url])
+
+  return (
+    <>
+      <Button onClick={() => handleShow()} style={{ color:"white", backgroundColor: "#395144", border: "none"}}>Read More...</Button>
+      {url.href[7] === "n" ? (
+        <Modal show={show} onHide={handleClose} fullscreen={true}>
+        <Modal.Header closeButton>
+          <div className="d-flex flex-column text-center">
+            {content.headlines && content.headlines.length > 0 ? (
+              <>
+                <Modal.Title>{content.headlines[0].title}</Modal.Title>
+                <div className="d-flex flex-row justify-content-around">
+                  <p className=""><em>~ by {content.headlines[0].source} ~</em></p>
+                  <p className=""><em>~ {formatTimestamp(content.timestamp)} ~</em></p>
+                </div>
+              </>
+            ) : (
+              <Modal.Title>No Title Available</Modal.Title>
+            )}
+          </div>
+        </Modal.Header>
+          {content.headlines && content.headlines.length > 0 ? (
+            <Modal.Body>
+              <div dangerouslySetInnerHTML={{ __html: content.headlines[0].story }}></div>
+            </Modal.Body>
+          ) : (
+            <Modal.Body>Error: No Content Available</Modal.Body>
+          )}
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ) : url.href[7] === "a" ? (
+        <Modal show={show} onHide={handleClose} fullscreen={true}>
+          <Modal.Header closeButton>
+          <div className="d-flex flex-column text-center">
+            {content.videos && content.videos.length > 0 ? (
+              <>
+                <Modal.Title>{content.videos[0].headline}</Modal.Title>
+                <div className="d-flex flex-row justify-content-around">
+                  <p className=""><em>~ {formatTimestamp(content.timestamp)} ~</em></p>
+                </div>
+              </>
+            ) : (
+              <Modal.Title>No Title Available</Modal.Title>
+            )}
+          </div>
+          </Modal.Header>
+          {content.videos && content.videos.length > 0 ? (
+            <>
+              <Modal.Body>
+                {content.videos[0].links && content.videos[0].links.mobile && content.videos[0].links.mobile.source.href && (
+                  <video controls src={content.videos[0].links.mobile.source.href} style={{ width: '100%' }}></video>
+                  )}
+                <p>{content.videos[0].description}</p>
+              </Modal.Body>
+            </>
+          ) : (
+            <Modal.Body>Error: No Content Available</Modal.Body>
+          )}
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+          </Modal.Footer>
+        </Modal>
+      ) : null}
+    </>
+  )
+}
+
+export default NewsModal;
